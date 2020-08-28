15a77f5f57047f14e740c5867066bbbe1e2d64e1
Bezrukov Yuriy <bezrukov-yra@mail.ru>
2020-08-28 20:06:43 +0300

    add css

--- script.js
+++ script.js
@@ -11,15 +11,12 @@ GET_COLUMNS_RESPONSE.forEach((column) => {
     let allCardsForColumnHtml = '';
 
     currentColumnCards.forEach(function (x) {
-        debugger
-        let userName = .....;
-    // Найти имя пользователя по его id, чтобы в карточках отображалось имя пользователя
-    // искать можно с помощью [1, 2, 3].find
-    allCardsForColumnHtml += getCard(x.title, x.description, userName);
-});
+        let user = GET_USERS_RESPONSE.find(user => user.id === x.userId);
+        allCardsForColumnHtml += getCard(x.title, x.description, user.name, user.photoUrl);
+    });
 
 
-htmlString += `<div class="col">
+    htmlString += `<div class="col">
                         <div class="col__header">${column.name}</div>
                         <div class="col_content">${allCardsForColumnHtml}</div>
                     </div>`;
@@ -34,13 +31,16 @@ function getCardsByColumnId(id) {
     })
 }
 
-function getCard(title, description, userName) {
+function getCard(title, description, userName, userPhoto) {
     return `<div class="card">
                 <div class="card__inform">
                     <div class="card__title">${title}</div>
                     <div class="card__description">${description}</div>
                 </div>
                 <div class="card__user">
+                    <div class="card__user-photo">
+                        <img src="${userPhoto}" >
+                    </div>
                     <div class="card__user-name">${userName}</div>
                 </div>
             </div>`;
